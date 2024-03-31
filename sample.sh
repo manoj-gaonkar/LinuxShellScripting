7715d4dac97127aa9f29f7c9aa949993b02e17b3
manoj-gaonkar <manojgaonkar7648@gmail.com>
2024-03-31 10:30:18 +0530

    add scripts'

---
+++ sample.sh
@@ -0,0 +1,7 @@
+#!/bin/env bash
+
+echo $1
+type  $*
+echo $@'$@'
+
+
---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+echo "manoj"
+date
+
+if [[ $1 ]]
+then
+if [ $1 == "m" ]
+then
+echo "there" $1
+fi
+fi
+sleep 2
+echo "manja"
