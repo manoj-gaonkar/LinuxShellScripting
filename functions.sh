425ee940812b08e9d2fbfdcc51427ee2a911cb9b
manoj-gaonkar <manojgaonkar7648@gmail.com>
2025-01-11 00:36:29 +0530

    positional params in functions

--- functions.sh
+++ functions.sh
@@ -3,10 +3,32 @@
 function add(){
 	echo "Hello $1"
 	hello
+	local LOCAL_VAR="local variable" #this is a local variable prefixed by local keyword
 }
 
 function hello(){
 	echo "i am hello function"
 }
-#function call
+#function call with positional parameter passed
 add manoj
+
+#the below cannot access LOCAL_VAR as it is a local varible declared with local in a function
+echo "$LOCAL_VAR"
+
+#------------------------------------------------------------------------
+# positional parameters
+# -----------------------------------------------------------------------
+
+function pos_params(){
+	for NAME in $@
+	do
+		echo "name: $NAME"
+	done
+
+	if [ $2 ]
+	then
+		echo "second name : $2"
+	fi
+}
+
+pos_params manoj 
